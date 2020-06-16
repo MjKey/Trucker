8d64596b0a8ec0e5e2d3734de9ec69e8eba9a9c2
MjKey <54132614+MjKey@users.noreply.github.com>
2020-06-17 01:20:09 +0300

    Add files via upload

---
+++ src/Overlay.NET.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Overlay.NET.Common;
+using Overlay.NET.Demo.Directx;
+using Overlay.NET.Demo.Internals;
+using Process.NET;
+
+namespace Overlay.NET.Demo {
+    /// <summary>
+    /// </summary>
+    public static class Program {
+        /// <summary>
+        ///     Defines the entry point of the application.
+        /// </summary>
+        [STAThread]
+        public static void Main() {
+            Log.Register("Console", new ConsoleLog());
+            var directXDemo = new DirectXOverlayDemo();
+            directXDemo.StartDemo();
+            Log.WriteLine("Оверлей запущен..");
+        }
+    }
+}
