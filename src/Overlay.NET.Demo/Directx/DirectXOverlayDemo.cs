30efcb4a72531f3018b3091ef5ddd0791661e788
MjKey <54132614+MjKey@users.noreply.github.com>
2020-07-03 04:41:20 +0300

    1,3

--- src/Overlay.NET.Demo/Directx/DirectXOverlayDemo.cs
+++ src/Overlay.NET.Demo/Directx/DirectXOverlayDemo.cs
@@ -1,25 +1,32 @@
+using Process.NET;
+using Process.NET.Memory;
 using System;
 using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
-using Overlay.NET.Common;
-using Process.NET;
-using Process.NET.Memory;
 
-namespace Overlay.NET.Demo.Directx {
-    public class DirectXOverlayDemo {
+namespace Overlay.NET.Demo.Directx
+{
+    public class DirectXOverlayDemo
+    {
         private OverlayPlugin _directXoverlayPluginExample;
         private ProcessSharp _processSharp;
 
-        public void StartDemo() {
-            var processName = "GTA5"; //имя процесса
+        public void StartDemo()
+        {
+            Console.Title = @"Время заказов для Дальнобойщика";
+            var processName = "notepad"; //имя процесса
+            //var processName = "GTA5"; //имя процесса
 
             var process = System.Diagnostics.Process.GetProcessesByName(processName).FirstOrDefault();
-            if (process == null) {
-                Log.Warn($"Игра не найдена.");
-                Log.Warn("Запустите сначало игру, а потом оверлей Дальнобойщика.");
-                Console.ReadKey();
-                return;
+            if (process == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Игра не найдена.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Ожидание запуска игры...");
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+                StartDemo();
             }
 
             _directXoverlayPluginExample = new DirectxOverlayPluginExample();
@@ -29,25 +36,36 @@ namespace Overlay.NET.Demo.Directx {
             var fpsValid = int.TryParse(Convert.ToString(result, CultureInfo.InvariantCulture), NumberStyles.Any,
                 NumberFormatInfo.InvariantInfo, out int fps);
 
-            var d3DOverlay = (DirectxOverlayPluginExample) _directXoverlayPluginExample;
+            var d3DOverlay = (DirectxOverlayPluginExample)_directXoverlayPluginExample;
             d3DOverlay.Settings.Current.UpdateRate = 1000 / fps;
             _directXoverlayPluginExample.Initialize(_processSharp.WindowFactory.MainWindow);
             _directXoverlayPluginExample.Enable();
 
             // Log some info about the overlay.
-            Log.Debug("Оверлей запущен");
 
             var info = d3DOverlay.Settings.Current;
 
-            Log.Info($"Автор: {info.Author}");
-            Log.Info($"Оверлей: {info.Identifier}");
-            Log.Info($"Версия: {info.Version}");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"Автор: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{ info.Author}");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"Оверлей: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{ info.Identifier}");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"Версия: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{ info.Version}");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Чтобы выключить оверлей - закройте консоль.");
 
-            Log.Info("Чтобы выключить оверлей - закройте консоль.");
-
-            while (true) {
+            while (true)
+            {
                 _directXoverlayPluginExample.Update();
             }
+
         }
     }
 }
--- src/Overlay.NET.Demo/Directx/DirectxOverlayPluginExample.cs
+++ src/Overlay.NET.Demo/Directx/DirectxOverlayPluginExample.cs
@@ -1,31 +1,44 @@
-using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.Security.Cryptography;
-using System.Threading;
 using Overlay.NET.Common;
 using Overlay.NET.Demo.Internals;
 using Overlay.NET.Directx;
 using Process.NET.Windows;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Media;
+using Color = System.Drawing.Color;
 
-namespace Overlay.NET.Demo.Directx {
-    [RegisterPlugin("Специально для Grand RP (GTA V)", "MjKey", "Время заказов для дальнобойщиков", "1.1 BETA",
+namespace Overlay.NET.Demo.Directx
+{
+    [RegisterPlugin("Специально для Grand RP (GTA V)", "MjKey", "Время заказов для дальнобойщиков", "1.3 BETA",
         "Тестовая версия.")]
-    public class DirectxOverlayPluginExample : DirectXOverlayPlugin {
+    public class DirectxOverlayPluginExample : DirectXOverlayPlugin
+    {
         private readonly TickEngine _tickEngine = new TickEngine();
         public readonly ISettings<DemoOverlaySettings> Settings = new SerializableSettings<DemoOverlaySettings>();
         private int _displayFps;
         private int _font;
+        private int fB;
         private int _hugeFont;
         private int _i;
         private int _interiorBrush;
         private int _redBrush;
+        private int _BlackBrush;
         private int _redOpacityBrush;
         private float _rotation;
         private Stopwatch _watch;
         private int r;
+        private int s;
+        private string ValS;
+        static double WI = System.Windows.SystemParameters.PrimaryScreenWidth;
+        private int WiGo = (int)(WI * 42 / 100);
 
-        public override void Initialize(IWindow targetWindow) {
+
+
+
+        public override void Initialize(IWindow targetWindow)
+        {
             // Set target window by calling the base method
             base.Initialize(targetWindow);
 
@@ -34,7 +47,9 @@ namespace Overlay.NET.Demo.Directx {
             var type = GetType();
 
             if (current.UpdateRate == 0)
+            {
                 current.UpdateRate = 1000 / 60;
+            }
 
             current.Author = GetAuthor(type);
             current.Description = GetDescription(type);
@@ -45,74 +60,137 @@ namespace Overlay.NET.Demo.Directx {
             // File is made from above info
             Settings.Save();
             Settings.Load();
-            Console.Title = @"Время заказов для Дальнобойщика";
+        Minuta:
+            Console.Clear();
+            Console.ResetColor();
             Console.WriteLine("Введите время следущего заказа (минуты)");
             Console.WriteLine("Например: 26");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Минуты: ");
             r = int.Parse(Console.ReadLine()); //ввод
             //r = 2;
+            if (r >= 0 && r <= 60)
+            {
+                Zvuk:
+                Console.Clear();
+                Console.WriteLine("Включить звуковое оповещение?");
+                Console.WriteLine("1 - Да | 0 - Нет (по умолчанию)");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Значение: ");
+                s = int.Parse(Console.ReadLine()); //ввод
+                if (s == 1 || s == 0)
+                {
+                    Console.Clear();
+                    void WriteLineCentered(string text)
+                    {
+                        int width = Console.WindowWidth;
+                        if (text.Length < width)
+                        {
+                            text = text.PadLeft((width - text.Length) / 2 + text.Length, ' ');
+                        }
+                        Console.WriteLine(text);
+                    }
+                    if (s == 1)
+                    {
+                        ValS = "включен";
+                    }
+                    else
+                    {
+                        ValS = "выключен";
+                    }
 
-            OverlayWindow = new DirectXOverlayWindow(targetWindow.Handle, false);
-            _watch = Stopwatch.StartNew();
+                    WriteLineCentered("ОВЕРЛЕЙ ЗАПУЩЕН | Начальная временная отметка: " + r + " | Звук " + ValS);
+                    if (s == 1)
+                    {
+                        System.Media.SoundPlayer SoundPlayer = new System.Media.SoundPlayer(Properties.Resources.coin04);
+                        SoundPlayer.Play();
+                    }
 
-            _redBrush = OverlayWindow.Graphics.CreateBrush(0x7FFF0000);
-            _redOpacityBrush = OverlayWindow.Graphics.CreateBrush(Color.FromArgb(220, 225, 0, 0));
-            _interiorBrush = OverlayWindow.Graphics.CreateBrush(0x7FFFFF00);
+                    OverlayWindow = new DirectXOverlayWindow(targetWindow.Handle, false);
+                    _watch = Stopwatch.StartNew();
 
-            _font = OverlayWindow.Graphics.CreateFont("Arial", 22, true);
-            _hugeFont = OverlayWindow.Graphics.CreateFont("Arial", 50, true);
+                    _redBrush = OverlayWindow.Graphics.CreateBrush(0x7FFF0000);
+                    _redOpacityBrush = OverlayWindow.Graphics.CreateBrush(Color.FromArgb(255, 225, 0, 0));
+                    _BlackBrush = OverlayWindow.Graphics.CreateBrush(Color.FromArgb(245, 0, 0, 0));
+                    _interiorBrush = OverlayWindow.Graphics.CreateBrush(0x7FFFFF00);
 
-            _rotation = 0.0f;
-            _displayFps = 0;
-            _i = 0;
-            // Set up update interval and register events for the tick engine.
+                    _font = OverlayWindow.Graphics.CreateFont("Arial", 22, true);
+                    fB = OverlayWindow.Graphics.CreateFont("Arial", 24, true);
+                    _hugeFont = OverlayWindow.Graphics.CreateFont("Arial", 50, true);
 
-            _tickEngine.PreTick += OnPreTick;
-            _tickEngine.Tick += OnTick;
+                    _rotation = 0.0f;
+                    _displayFps = 0;
+                    _i = 0;
+                    // Set up update interval and register events for the tick engine.
+
+                    _tickEngine.PreTick += OnPreTick;
+                    _tickEngine.Tick += OnTick;
+                }
+                else
+                {
+                    goto Zvuk;
+                }
+            }
+            else
+            {
+                goto Minuta;
+            }
+           
         }
 
-        private void OnTick(object sender, EventArgs e) {
-            if (!OverlayWindow.IsVisible) {
-                return;
+        private void OnTick(object sender, EventArgs e)
+        {
+            if (!OverlayWindow.IsVisible)
+            {
+                OverlayWindow.Update();
             }
 
             OverlayWindow.Update();
             InternalRender();
+
         }
 
-        private void OnPreTick(object sender, EventArgs e) {
+        private void OnPreTick(object sender, EventArgs e)
+        {
             var targetWindowIsActivated = TargetWindow.IsActivated;
-            if (!targetWindowIsActivated && OverlayWindow.IsVisible) {
-                _watch.Stop();
-                ClearScreen();
+            if (!targetWindowIsActivated && OverlayWindow.IsVisible)
+            {
+               // _watch.Stop();
+               // ClearScreen();
                 OverlayWindow.Hide();
             }
-            else if (targetWindowIsActivated && !OverlayWindow.IsVisible) {
+            else if (targetWindowIsActivated && !OverlayWindow.IsVisible)
+            {
                 OverlayWindow.Show();
+                
             }
         }
 
         // ReSharper disable once RedundantOverriddenMember
-        public override void Enable() {
+        public override void Enable()
+        {
             _tickEngine.Interval = Settings.Current.UpdateRate.Milliseconds();
             _tickEngine.IsTicking = true;
             base.Enable();
         }
 
+
         // ReSharper disable once RedundantOverriddenMember
-        public override void Disable() {
+        public override void Disable()
+        {
             _tickEngine.IsTicking = false;
             base.Disable();
         }
 
         public override void Update() => _tickEngine.Pulse();
 
-
-        protected void InternalRender() {
-            if (!_watch.IsRunning) {
+        protected void InternalRender()
+        {
+            /*if (!_watch.IsRunning)
+            {
                 _watch.Start();
-            }
-
+            }*/
+            _watch.Start();
             OverlayWindow.Graphics.BeginScene();
             OverlayWindow.Graphics.ClearScene();
 
@@ -123,6 +201,11 @@ namespace Overlay.NET.Demo.Directx {
             if (r == m)
             {
                 r = m + 7;
+                if (s == 1)
+                {
+                    System.Media.SoundPlayer SoundPlayerX = new System.Media.SoundPlayer(Properties.Resources.x);
+                    SoundPlayerX.Play();
+                }
             }
 
             if (r >= 60)
@@ -186,7 +269,10 @@ namespace Overlay.NET.Demo.Directx {
 
 
             //OverlayWindow.Graphics.DrawText(time, _font, _redBrush, 400, 40);
-            OverlayWindow.Graphics.DrawText("Следующий заказ: " + ts, _font, _redOpacityBrush, 800, 30);
+
+            OverlayWindow.Graphics.DrawText("Следующий заказ: " + ts, fB, _BlackBrush, WiGo+2, 30);
+
+            OverlayWindow.Graphics.DrawText("Следующий заказ: " + ts, _font, _redOpacityBrush, WiGo, 30);
 
             _rotation += 0.03f; //related to speed
 
@@ -195,27 +281,33 @@ namespace Overlay.NET.Demo.Directx {
                 _rotation = -50.0f;
             }
 
-            if (_watch.ElapsedMilliseconds > 1000) {
+            if (_watch.ElapsedMilliseconds > 1000)
+            {
                 _i = _displayFps;
                 _displayFps = 0;
                 _watch.Restart();
             }
 
-            else {
+            else
+            {
                 _displayFps++;
             }
 
             //OverlayWindow.Graphics.DrawText("FPS: " + _i, _hugeFont, _redBrush, 840, 600, false);
 
             OverlayWindow.Graphics.EndScene();
+
         }
 
-        public override void Dispose() {
+
+public override void Dispose()
+        {
             OverlayWindow.Dispose();
             base.Dispose();
         }
 
-        private void ClearScreen() {
+        private void ClearScreen()
+        {
             OverlayWindow.Graphics.BeginScene();
             OverlayWindow.Graphics.ClearScene();
             OverlayWindow.Graphics.EndScene();
